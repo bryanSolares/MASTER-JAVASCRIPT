e0103f9d01f774f22210f9a44f7dd53f26c543c6
Linux-mintSolares <solares.josue@outlook.com>
2020-02-25 05:56:26 -0600

    Se carga hasta video 83

--- DOCUMENTACION OFICIAL/bucles_iteraciones.js
+++ DOCUMENTACION OFICIAL/bucles_iteraciones.js
@@ -1,2 +1,140 @@
-"use strict"
 
+for (let step = 0; step < 5; step++) {
+    console.log("Walking ease one step");
+}
+
+function howMany(selectObject) {
+    var numberSelected = 0;
+    for (var i = 0; i < selectObject.options.length; i++) {
+        if (selectObject.options[i].selected) {
+            numberSelected++;
+        }
+    }
+
+    return numberSelected;
+}
+
+var btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+    alert("Número de opciones seleccionadas: " + howMany(document.selectForm.musicTypes));
+});
+
+
+//do while
+var i = 0;
+
+do {
+    i += 1;
+    console.log(i);
+} while (i < 5);
+
+//while
+n = 0;
+x = 0;
+
+while (n < 3) {
+    n++;
+    x += n;
+}
+
+console.log("valor de n:", n, "valor de x:", x);
+
+//label
+/*
+miLabel:
+while(theMark == true){
+    doSomething();
+}*/
+
+
+//break
+/*
+for (i = 0; i < a.longitud; i++) {
+    if (a[i] == elValor) {
+        break;
+    }
+}*/
+
+
+//debugger;
+var x = 0;
+var z = 0;
+labelCancelLoop: while (true) {
+    console.log("Outer loops", x);
+    x++;
+    z = 1;
+    while (true) {
+        console.log("Inner loops:", z);
+        z++;
+        if (z === 10 && x === 10) {
+            break labelCancelLoop;
+        } else if (z === 10) {
+            break;
+        }
+    }
+}
+
+//debugger;
+var i = 0;
+var n = 0;
+while (i < 5) {
+    i++;
+    if (i == 3) {
+        continue;
+    }
+    n += i;
+}
+
+//debugger;
+var i = 0;
+var j = 20;
+
+checkiandj:
+while ( i < 4){
+    console.log(i);
+    i++;
+    checkj:
+    while(j > 4){
+        console.log(j);
+        j--;
+        if((j%2)==0){
+            continue checkj;
+        }
+        console.log(j,"is odd.");
+    }
+    console.log("i =",i);
+    console.log("j =",j);
+}
+
+
+//for ... in = itera sobre la propiedades definidas por el usuario en un array en orden arbitrario
+function nombre_propiedades(obj, obj_nombre){
+    var resultado = "";
+    for(let i in obj){
+        resultado += obj_nombre + "." + i + " = " + obj[i] + "<br>";
+    }
+    resultado += "<hr>";
+    return resultado;
+}
+
+var obj = {a: 1, b: 2, c:3};
+
+for(const nombre  in obj){
+    console.log(`nombre.${nombre} = valor.${obj[nombre]}`);
+}
+
+
+//for .. of = bucle iterando sobre objetos iterables (array, map, set, argumentos, etc )
+//for .. in = sobre nombres de propiedades - for ... of = valores de propiedades
+let arreglo = [3, 5, 6];
+arreglo.foo = "hello";
+
+//valores
+for(let i of arreglo){
+    console.log(i);
+}
+
+//nombres
+for(let i in arreglo){
+    console.log(i);
+}
---
+++ JAVASCRIPT-PROFUNDIDAD/PETICIONES ASINCRONAS/peticiones.js
@@ -0,0 +1,77 @@
+"use strict"
+var listaUsuarios = document.querySelector("#usuarios");
+var Janet = document.querySelector("#janet");
+
+//debugger;
+obtenerJson("https://reqres.in/api/users")
+    .then(data =>
+        data.json())
+    .then(users => {
+
+        mostrarLista(users.data, listaUsuarios);
+        return obtenerJson("https://reqres.in/api/users/2");
+
+    }).then(data =>
+        data.json())
+    .then(user => {
+
+        imprimirUno(user.data, Janet);
+        return getInfo();
+
+    }).then(data => {
+        console.log(data);
+    }).catch(error =>{
+        alert("Error en las peticiones " + error);
+    });
+
+
+
+function obtenerJson(direccionURL) {
+    return fetch(direccionURL);
+}
+
+function mostrarLista(usuarios, elemento) {
+    usuarios.map((user) => {
+        adherirAElemento(user, elemento)
+        document.querySelector(".loading").style.display = "none";
+    });
+}
+
+function imprimirUno(dato, elemento) {
+    adherirAElemento(dato, elemento)
+    document.querySelector("#janet .loading").style.display = "none";
+}
+
+function adherirAElemento(usuario, elemento) {
+    let nombre = document.createElement("h2");
+    let avatar = document.createElement("img");
+
+    nombre.innerHTML = usuario.first_name + " " + usuario.last_name + " EMAIL: " + usuario.email;
+    avatar.src = usuario.avatar;
+    avatar.width = "100";
+
+    elemento.appendChild(nombre);
+    elemento.appendChild(avatar);
+}
+
+//Creación de promesas
+function getInfo() {
+    var profesor = {
+        nombres: "Bryan Josue",
+        apellidos: "Solares Perez",
+        url: "www.cualquiercosa.com"
+    };
+
+    return new Promise((resolve, reject) => {
+        var profesorString = JSON.stringify(profesor);
+
+        if (typeof profesorString != "string") {
+            return reject("Error");
+        } else {
+            setTimeout(() => {
+                return resolve(profesorString);
+            }, 3000);
+
+        }
+    });
+}
