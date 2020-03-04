5e0fde642e4e8d148d22ab313d48997351ba7e28
Linux-mintSolares <solares.josue@outlook.com>
2020-03-03 23:15:42 -0600

    Se carga hasta video 121

--- JAVASCRIPT-PROFUNDIDAD/JQUERY/JQUERY UI/jquery-ui.js
+++ JAVASCRIPT-PROFUNDIDAD/JQUERY/JQUERY UI/jquery-ui.js
@@ -1,13 +1,15 @@
-$(document).ready(()=>{
-    
+$(document).ready(() => {
+
     var elemento = $(".elemento");
     var lista = $(".selected-list");
     var area = $("#area");
     var elemento_movible = $("#elemento-movible");
+    var boton_mostrar = $("#mostrar");
+    var caja = $(".caja-efectos");
 
     //elemento movible
-    elemento.draggable(); 
-    
+    elemento.draggable();
+
     //elemento redimensionable
     elemento.resizable();
 
@@ -16,7 +18,7 @@ $(document).ready(()=>{
 
     //Ordenar elementos
     lista.sortable({
-        update: function(evento){
+        update: function (evento) {
             console.log(evento);
         }
     });
@@ -24,9 +26,31 @@ $(document).ready(()=>{
     //Drop - Droppable
     elemento_movible.draggable();
     area.droppable({
-        drop: function(){
+        drop: function () {
             console.log("Has soltado algo dentro del area");
         }
     });
+
+    //debugger;
+    //efectos
+    boton_mostrar.click(function () {
+        //caja.fadeToggle();
+        //caja.toggle("explode");
+        //caja.toggle("drop");
+        //caja.toggle("fold");
+        caja.toggle("shake", {}, 5000);
+    });
+
+    //tooltip
+    $(document).tooltip();
+
+    //dialog    
+    $("#lanzame").click(function () {
+        $("#cuadro-dialogo").dialog();
+    });
+
+    $("#calendario").datepicker();
+
+    $("#tabs").tabs();
 
 });
