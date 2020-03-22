9ba79bcd36dfce8cc4b662ea5fb4ae32de639657
Bryan Solares i7 <solares.josue@outlook.com>
2020-03-22 01:51:05 -0600

    Se finaliza la aplicacion de links

--- NODEJS-MYSQL-APP/src/routes/links.js
+++ NODEJS-MYSQL-APP/src/routes/links.js
@@ -1,44 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../database");
-const { route } = require(".");
+const {isLoggedIn} = require("../lib/auth")
 
-router.get("/add",(req, resp)=>{
+router.get("/add", isLoggedIn, (req, resp)=>{
     resp.render("links/add")
 }); 
 
-router.post("/add",async(req, res)=>{
+router.post("/add", isLoggedIn, async(req, res)=>{
     const {title, url, description} =  req.body;
     const newLink = {
         title,
         url,
-        description
+        description,
+        user_id: req.user.id
     }; 
     await pool.query("INSERT INTO links SET ?",[newLink]);
     req.flash("success","Link saved successfully");
     res.redirect("/links");
 }); 
 
-router.get("/",async(req, res)=>{
-    const links = await pool.query("SELECT * FROM links");
-    console.log(links)
+router.get("/", isLoggedIn, async(req, res)=>{
+    const links = await pool.query("SELECT * FROM links WHERE user_id = ?",[req.user.id]);
     res.render("links/list",{links});
 })
 
-router.get("/delete/:id",async(req,res)=>{
+router.get("/delete/:id", isLoggedIn, async(req,res)=>{
     const {id} = req.params;
     await pool.query("DELETE FROM links WHERE id = ?",[id]);
     req.flash("success","Link removed successfully")
     res.redirect("/links");
 });
 
-router.get("/edit/:id",async(req,res)=>{
+router.get("/edit/:id", isLoggedIn, async(req,res)=>{
     const {id} = req.params;
     const link = await pool.query("SELECT * FROM links WHERE id = ?",[id]);
     res.render("links/edit",{link:link[0]});
 });
 
-router.post("/edit/:id",async(req,res)=>{
+router.post("/edit/:id", isLoggedIn, async(req,res)=>{
     const {id} = req.params;
     const {title, url, description} = req.body;
     const newLinks ={
