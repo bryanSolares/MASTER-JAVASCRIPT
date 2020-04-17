2803caa90e5fade1fdb94c10e83a7d72401f499f
Bryan Solares i7 <solares.josue@outlook.com>
2020-04-17 13:59:52 -0600

    Se agrega hasta inicio de MEAN

--- ANGULAR/primerproyecto/src/app/cursos/cursos.component.ts
+++ ANGULAR/primerproyecto/src/app/cursos/cursos.component.ts
@@ -1,18 +1,33 @@
-//Uno de los Hooks más importantes es el componente OnInit
-
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
-  selector: 'app-cursos',
+  selector: 'cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+	public nombre: string;
+	public followers: number;
 
-  constructor() { }
+	constructor(
+		private _route: ActivatedRoute,
+		private _router: Router
+	){ }
 
-  //Método que se ejecuta al cargar el componente
-  ngOnInit(): void {
-  }
+	ngOnInit() {
+		this._route.params.subscribe((params: Params) => {
+			this.nombre = params.nombre;
+			this.followers = +params.followers;
+
+			if(this.nombre == 'ninguno'){
+				this._router.navigate(['/home']);
+			}
+		});
+	}
+
+	redirigir(){
+		this._router.navigate(['/zapatillas']);
+	}
 
 }
